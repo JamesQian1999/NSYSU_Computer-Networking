5c9b4d5cbb0e42b33be830d38dcfe72b7ab2b612
jamesqian <jamesqian1999@gmail.com>
2021-05-25 01:09:02 +0800

    DNS_finish

--- my/client.cpp
+++ my/client.cpp
@@ -18,23 +18,15 @@ using namespace std;
 class Package
 {
 public:
-    unsigned short int destination_port = 0;
-    unsigned short int source_port = 0;
+    unsigned short destination_port = 0;
+    unsigned short source_port = 0;
     unsigned int seq_num = 0;
     unsigned int ack_num = 0;
     unsigned char header_length = 0;
-    unsigned char reserved1 = 0;
-    bool reserved2 = 0;
-    bool reserved3 = 0;
-    bool URG = 0;
     bool ACK = 0;
-    bool PSH = 0;
-    bool PST = 0;
     bool SYN = 0;
     bool FIN = 0;
     unsigned short window_size = 0;
-    unsigned short check_sum = 0;
-    unsigned short urgent_pointer = 0;
     char data[1024];
 };
 
@@ -45,18 +37,10 @@ void reset(Package *p)
     p->seq_num = 0;
     p->ack_num = 0;
     p->header_length = 0;
-    p->reserved1 = 0;
-    p->reserved2 = 0;
-    p->reserved3 = 0;
-    p->URG = 0;
     p->ACK = 0;
-    p->PSH = 0;
-    p->PST = 0;
     p->SYN = 0;
     p->FIN = 0;
     p->window_size = 0;
-    p->check_sum = 0;
-    p->urgent_pointer = 0;
     memset(&p->data, 0, sizeof p->data);
 }
 
@@ -64,13 +48,13 @@ int main(int argc, char *argv[])
 {
     char SERVERPORT[5] = "4950"; // 使用者所要連線的 port
     int sockfd;
-    struct addrinfo hints, *servinfo, *p;
+    struct addrinfo hints, *servinfo;
     int rv;
     int numbytes;
 
     if (argc < 2 || argc % 2) //172.20.10.7 -f 1.mp4 -d www.google.com
     {
-        fprintf(stderr, "Invaild format.\n");
+        printf("Invaild format.\n");
         exit(1);
     }
 
@@ -80,32 +64,26 @@ int main(int argc, char *argv[])
 
     if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0)
     {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+        printf("getaddrinfo error\n");
+        exit(1);
     }
 
-    p = servinfo;
-    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
     {
-        perror("talker: socket");
+        printf("Server socket error\n");
+        exit(1);
     }
     freeaddrinfo(servinfo);
 
-    if (p == NULL)
-    {
-        fprintf(stderr, "talker: failed to bind socket\n");
-        return 2;
-    }
-
     //3 way handshake
-    Package handshake;
+    printf("\033[33m=====Start the three-way handshake======\033[m\n");
+    printf("Sent package(SYN) to %s : %s\n", argv[1], SERVERPORT);
+    Package handshake, package;
     handshake.SYN = 1;
     handshake.seq_num = rand() % 10000 + 1;
-    cout << "before sockfd = " << sockfd << endl;
-    printf("sent ==> seq num: %d\n", handshake.seq_num);
-    if ((numbytes = sendto(sockfd, (char *)&handshake, sizeof(handshake), 0, p->ai_addr, p->ai_addrlen)) == -1)
+    if ((numbytes = sendto(sockfd, (char *)&handshake, sizeof(handshake), 0, servinfo->ai_addr, servinfo->ai_addrlen)) == -1)
     {
-        perror("3-Way-Handshake ERROR\n");
+        printf("3-Way-Handshake ERROR\n");
         exit(1);
     }
     else
@@ -114,17 +92,25 @@ int main(int argc, char *argv[])
         struct sockaddr_storage their_addr;
         socklen_t their_addr_len;
         numbytes = recvfrom(sockfd, (char *)&handshake, sizeof(handshake), 0, (struct sockaddr *)&their_addr, &their_addr_len);
-        printf("receive ==> ack num:%d, seq num: %d, new port: %s\n", handshake.ack_num, handshake.seq_num, handshake.data);
+        printf("Receive package(SYN/ACK) from %s : %s\n", argv[1], SERVERPORT);
+        printf("\tReceive a package ( seq_num = %u, ack_num = %u )\n", handshake.seq_num, handshake.ack_num);
+        printf("\033[33m====Complete the three-way handshake====\033[m\n");
 
         strncpy(SERVERPORT, handshake.data, sizeof(SERVERPORT) - 1);
-        cout << "port: " << SERVERPORT << endl;
-        cout << getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo) << endl;
+        getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo);
         sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-        cout << "after sockfd = " << sockfd << endl;
 
-        strncpy(handshake.data, "hello world\n", sizeof(handshake.data) - 1);
-        sendto(sockfd, (char *)&handshake, sizeof(handshake), 0, servinfo->ai_addr, servinfo->ai_addrlen);
-        cout << "here" << endl;
+        //sent request
+        char num[50] = {0};
+        sprintf(num, "%d", (argc - 2) / 2);     //caculate request num 
+        strcat(package.data, num);              //request num
+        for (int i = 2; i < argc; i++)
+        {
+            strcat(package.data, " ");
+            strcat(package.data, argv[i]);
+        }
+
+        sendto(sockfd, (char *)&package, sizeof(package), 0, servinfo->ai_addr, servinfo->ai_addrlen);
     }
 
     while (1)
@@ -145,8 +131,6 @@ int main(int argc, char *argv[])
         struct sockaddr_storage their_addr;
         socklen_t their_addr_len = sizeof their_addr;
         //numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0, (struct sockaddr *)&their_addr, &their_addr_len);
-        buf[numbytes] = '\0';
-        //printf("Receive : %s\n", buf);
     }
 
     close(sockfd);
--- my/server.cpp
+++ my/server.cpp
@@ -21,23 +21,15 @@ kill -9 19422
 class Package
 {
 public:
-    unsigned short int destination_port = 0;
-    unsigned short int source_port = 0;
+    unsigned short destination_port = 0;
+    unsigned short source_port = 0;
     unsigned int seq_num = 0;
     unsigned int ack_num = 0;
     unsigned char header_length = 0;
-    unsigned char reserved1 = 0;
-    bool reserved2 = 0;
-    bool reserved3 = 0;
-    bool URG = 0;
     bool ACK = 0;
-    bool PSH = 0;
-    bool PST = 0;
     bool SYN = 0;
     bool FIN = 0;
     unsigned short window_size = 0;
-    unsigned short check_sum = 0;
-    unsigned short urgent_pointer = 0;
     char data[1024];
 };
 
@@ -48,30 +40,30 @@ void reset(Package *p)
     p->seq_num = 0;
     p->ack_num = 0;
     p->header_length = 0;
-    p->reserved1 = 0;
-    p->reserved2 = 0;
-    p->reserved3 = 0;
-    p->URG = 0;
     p->ACK = 0;
-    p->PSH = 0;
-    p->PST = 0;
     p->SYN = 0;
     p->FIN = 0;
     p->window_size = 0;
-    p->check_sum = 0;
-    p->urgent_pointer = 0;
     memset(&p->data, 0, sizeof p->data);
 }
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+char *DNS(char url[888], char *ipstr)
 {
-    if (sa->sa_family == AF_INET)
-    {
-        return &(((struct sockaddr_in *)sa)->sin_addr);
-    }
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+    printf("url: %s\n", url);
+    getaddrinfo(url, NULL, &hints, &servinfo);
+
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)servinfo->ai_addr;
+    void *addr = &(ipv4->sin_addr);
+    inet_ntop(hints.ai_family, addr, ipstr, INET6_ADDRSTRLEN);
+    printf("IP(func): %s\n", ipstr);
+    return ipstr;
 }
 
 int main(void)
@@ -94,65 +86,48 @@ int main(void)
 
     if ((rv = getaddrinfo(NULL, SERVERPORT, &hints, &servinfo)) != 0)
     {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+        printf("Server getaddrinfo error\n");
+        exit(1);
     }
 
-    // 用迴圈來找出全部的結果，並 bind 到首先找到能 bind 的
-    for (p = servinfo; p != NULL; p = p->ai_next)
+    if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
     {
-
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-        {
-            perror("listener: socket");
-            continue;
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-        {
-            close(sockfd);
-            perror("listener: bind");
-            continue;
-        }
-
-        break;
+        printf("Server socket error\n");
+        exit(1);
     }
 
-    if (p == NULL)
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
     {
-        fprintf(stderr, "listener: failed to bind socket\n");
-        return 2;
+        close(sockfd);
+        printf("Server bind error\n");
+        exit(1);
     }
-
     freeaddrinfo(servinfo);
-    printf("listener: waiting to recvfrom...\n");
+
+    printf("\033[33mServer is ready......\033[m\n\n");
     their_addr_len = sizeof their_addr;
 
     while (1)
     {
         Package package;
-        cout << "parent socket: " << sockfd << endl;
+        // 3 way handshake (receive SYN)
         if ((numbytes = recvfrom(sockfd, (char *)&package, sizeof(package), 0, (struct sockaddr *)&their_addr, &their_addr_len)) == -1)
         {
             perror("Receive ERROR\n");
-            exit(1);
+            continue;
         }
 
+        // creat a port for client
         int tmp = atoi(SERVERPORT);
         sprintf(SERVERPORT, "%d", tmp + 1);
 
+        // let child process handle client
         int pid = fork();
 
         if (!pid)
         {
-            printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
-            printf("listener: packet is %d bytes long\n", numbytes);
-
-            buf[numbytes] = '\0';
-            if (package.SYN)
-                printf("receive SYN\n");
-            else
-                printf("Receive : %s\n", package.data);
+            //printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, &(((struct sockaddr_in *)&their_addr)->sin_addr), s, sizeof s));
+            printf("\tReceive a package (SYN)\n");
 
             int client_isn = package.seq_num;
             reset(&package);
@@ -161,45 +136,50 @@ int main(void)
             package.ack_num = client_isn + 1;
             strncpy(package.data, SERVERPORT, sizeof(package.data));
             sendto(sockfd, (char *)&package, sizeof(package), 0, (struct sockaddr *)&their_addr, their_addr_len);
-
-            struct addrinfo hints, *servinfo, *p;
-            int rv, numbytes;
-            struct sockaddr_storage their_addr;
-            socklen_t their_addr_len;
-            char s[INET6_ADDRSTRLEN];
-
-            memset(&hints, 0, sizeof hints);
-
-            hints.ai_family = AF_INET; // IPv4
-            hints.ai_socktype = SOCK_DGRAM;
-            hints.ai_flags = AI_PASSIVE; // 使用我的 IP
+            // 3 way handshake finish
 
             if ((rv = getaddrinfo(NULL, SERVERPORT, &hints, &servinfo)) != 0)
             {
-                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-                return 1;
+                printf("Connet getaddrinfo error\n");
+                exit(1);
             }
 
-            // 用迴圈來找出全部的結果，並 bind 到首先找到能 bind 的
-            p = servinfo;
-            if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+            if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
             {
-                perror("listener: socket");
+                printf("Connet socket error\n");
+                exit(1);
             }
 
-            if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+            if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
             {
                 close(sockfd);
-                perror("listener: bind");
+                printf("Connet bind error\n");
+                exit(1);
             }
 
             numbytes = recvfrom(sockfd, (char *)&package, sizeof(package), 0, (struct sockaddr *)&their_addr, &their_addr_len);
-            printf("Receive: %s\n", package.data);
+            printf("\tReceive a package ( seq_num = %u, ack_num = %u )\n", package.seq_num, package.ack_num);
+
+            int num = 0;
+            sscanf(package.data, "%d", &num); // receive request num
+
+            /* test DNS */
+            char *pch = strtok(package.data, " ");
+            for (int i = 0; i < 2*num; i++)
+            {
+                pch = strtok(NULL, " ");
+                printf("%s\n", pch);
+            }
+
+            char ipstr[INET6_ADDRSTRLEN];
+            printf("IP: %s\n", DNS(pch, ipstr));
+            /* test DNS */
 
             while (1)
             {
                 numbytes = recvfrom(sockfd, (char *)&package, sizeof(package), 0, (struct sockaddr *)&their_addr, &their_addr_len);
-                printf("Receive: %s\n", package.data);
+                printf("\tReceive a package ( seq_num = %u, ack_num = %u )\n", package.seq_num, package.ack_num);
+                printf("\tReceive : %s\n", package.data);
             }
 
             exit(0);
